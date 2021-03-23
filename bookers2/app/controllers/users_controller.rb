18c65a24da81a3966ba8bda63f74ed056ce51c77
masatonara <masato.nara83@gmail.com>
2021-03-23 12:37:07 +0000

    [finish]bookers2

---
+++ bookers2/app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+before_action :authenticate_user!
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def show
+    @user = User.find(params[:id]) 
+    @books = @user.books.reverse_order
+    @book = Book.new
+  end
+  
+  def edit
+     @user = User.find(params[:id])
+      if @user == current_user
+        render :edit
+      else
+        redirect_to user_path(current_user)
+      end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'successfully'
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+end
---
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '81a4d9f3541fb509fbb5bb2a486603845ff5098617d5ec2e1b372d8ac81e9692d82b837bc980258a495096a61e8db56e24fb84593e35a9361001932f5d7fc94d'
